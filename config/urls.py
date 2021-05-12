4d8f5208aab84627df43105c8e52ed8be97e12ed
SemenovAV <7.on.off@bk.ru>
2021-05-12 14:06:27 +0300

    refactor: admin url

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("srv/admin/", admin.site.urls),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
