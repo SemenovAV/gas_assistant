de12cd4f000e3151653589805e05ad70423cb71f
SemenovAV <7.on.off@bk.ru>
2021-05-13 15:34:17 +0300

    refactor: url

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("srv/admin/", admin.site.urls),
-    path('', include('demo.urls', namespace='indormer')),
+    path('srv/', include('demo.urls', namespace='indormer')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
