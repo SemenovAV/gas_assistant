1f5fa244d72b802b552ab1c505d5ecbc54a44bc0
SemenovAV <7.on.off@bk.ru>
2021-05-29 16:29:30 +0300

    fix: redis url

--- config/settings.py
+++ config/settings.py
@@ -162,18 +162,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Celery
 # REDIS settings
-if os.environ.get('REDIS_URL'):
-    REDIS = dj_redis_url.config()
-    REDIS_HOST = REDIS['HOST']
-    REDIS_PORT = str(REDIS['PORT'])
-else:
+REDIS_URL = os.environ.get('REDIS_URL')
+if not REDIS_URL:
     REDIS_HOST = os.environ.get('REDIS_HOST', '127.0.0.1')
     REDIS_PORT = os.environ.get('REDIS_PORT', '6379')
-
+    REDIS_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
 # CELERY settings
-CELERY_BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+CELERY_BROKER_URL = REDIS_URL + '/0'
 CELERY_BROKER_TRANSPORT_OPTION = {'visibility_timeout': 3600}
-CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+CELERY_RESULT_BACKEND = REDIS_URL + '/0'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
